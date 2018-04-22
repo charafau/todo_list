56c31086ea864d8f32391addf891c7349c063d89
charafau <gorudonu@gmail.com>
2018-04-22 20:22:30 +0100

    setup built_value

---
+++ lib/todo.dart
@@ -0,0 +1,18 @@
+library todo;
+
+import 'package:built_value/built_value.dart';
+
+part 'todo.g.dart';
+
+
+abstract class Todo implements Built<Todo, TodoBuilder> {
+
+  Todo._();
+
+  @BuiltValueField(wireName: 'title')
+  String get title;
+
+  factory Todo([updates(TodoBuilder b)]) = _$Todo;
+
+
+}
---
+++ lib/todo.g.dart
@@ -0,0 +1,88 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of todo;
+
+// **************************************************************************
+// Generator: BuiltValueGenerator
+// **************************************************************************
+
+// ignore_for_file: always_put_control_body_on_new_line
+// ignore_for_file: annotate_overrides
+// ignore_for_file: avoid_annotating_with_dynamic
+// ignore_for_file: avoid_returning_this
+// ignore_for_file: omit_local_variable_types
+// ignore_for_file: prefer_expression_function_bodies
+// ignore_for_file: sort_constructors_first
+
+class _$Todo extends Todo {
+  @override
+  final String title;
+
+  factory _$Todo([void updates(TodoBuilder b)]) =>
+      (new TodoBuilder()..update(updates)).build();
+
+  _$Todo._({this.title}) : super._() {
+    if (title == null) throw new BuiltValueNullFieldError('Todo', 'title');
+  }
+
+  @override
+  Todo rebuild(void updates(TodoBuilder b)) =>
+      (toBuilder()..update(updates)).build();
+
+  @override
+  TodoBuilder toBuilder() => new TodoBuilder()..replace(this);
+
+  @override
+  bool operator ==(dynamic other) {
+    if (identical(other, this)) return true;
+    if (other is! Todo) return false;
+    return title == other.title;
+  }
+
+  @override
+  int get hashCode {
+    return $jf($jc(0, title.hashCode));
+  }
+
+  @override
+  String toString() {
+    return (newBuiltValueToStringHelper('Todo')..add('title', title))
+        .toString();
+  }
+}
+
+class TodoBuilder implements Builder<Todo, TodoBuilder> {
+  _$Todo _$v;
+
+  String _title;
+  String get title => _$this._title;
+  set title(String title) => _$this._title = title;
+
+  TodoBuilder();
+
+  TodoBuilder get _$this {
+    if (_$v != null) {
+      _title = _$v.title;
+      _$v = null;
+    }
+    return this;
+  }
+
+  @override
+  void replace(Todo other) {
+    if (other == null) throw new ArgumentError.notNull('other');
+    _$v = other as _$Todo;
+  }
+
+  @override
+  void update(void updates(TodoBuilder b)) {
+    if (updates != null) updates(this);
+  }
+
+  @override
+  _$Todo build() {
+    final _$result = _$v ?? new _$Todo._(title: title);
+    replace(_$result);
+    return _$result;
+  }
+}
